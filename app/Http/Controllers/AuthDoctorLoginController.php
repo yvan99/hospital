d1f64722f9b133ffdb778f45e811fe4d72ec67b4
yvan99 <ishimweyvan90@gmail.com>
2023-06-03 18:49:10 +0200

    add feature

--- app/Http/Controllers/AuthDoctorLoginController.php
+++ app/Http/Controllers/AuthDoctorLoginController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class AuthDoctorLoginController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    protected $redirectTo = '/doctor/dashboard';
+
+    public function __construct()
+    {
+        $this->middleware('guest:doctor')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.doctor-login');
+    }
+
+    protected function guard()
+    {
+        return auth()->guard('doctor');
+    }
 }
--- app/Http/Controllers/AuthNurseLoginController.php
+++ app/Http/Controllers/AuthNurseLoginController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class AuthNurseLoginController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    protected $redirectTo = '/nurse/dashboard';
+
+    public function __construct()
+    {
+        $this->middleware('guest:nurse')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.nurse-login');
+    }
+
+    protected function guard()
+    {
+        return auth()->guard('nurse');
+    }
 }
