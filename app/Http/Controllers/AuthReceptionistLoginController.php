92f7ff7576fbf8e0a8cb794f28fda9620548ed3f
yvan99 <ishimweyvan90@gmail.com>
2023-06-03 18:47:49 +0200

    add feature

--- app/Http/Controllers/AuthReceptionistLoginController.php
+++ app/Http/Controllers/AuthReceptionistLoginController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class AuthReceptionistLoginController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    protected $redirectTo = '/receptionist/dashboard';
+
+    public function __construct()
+    {
+        $this->middleware('guest:receptionist')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.receptionist-login');
+    }
+
+    protected function guard()
+    {
+        return auth()->guard('receptionist');
+    }
 }
