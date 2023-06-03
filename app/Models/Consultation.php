bb978c896936a0eeb456da7de28939c723917ff8
yvan99 <ishimweyvan90@gmail.com>
2023-06-03 14:00:57 +0200

    consultation model

---
+++ app/Models/Consultation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Consultation extends Model
+{
+    use HasFactory;
+
+    public function patientOrder()
+    {
+        return $this->belongsTo(PatientOrder::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function nurse()
+    {
+        return $this->belongsTo(Nurse::class);
+    }
+}
