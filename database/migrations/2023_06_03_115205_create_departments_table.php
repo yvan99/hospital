59d2d0ff4be5d512bde8f85ce670678ca5f651fb
yvan99 <ishimweyvan90@gmail.com>
2023-06-03 13:53:34 +0200

    depts migrations

--- database/migrations/2023_06_03_115205_create_departments_table.php
+++ database/migrations/2023_06_03_115205_create_departments_table.php
@@ -6,24 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateDepartmentsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('departments');
