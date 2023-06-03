2897cf2d6c0994e992148e665012ded5439873de
yvan99 <ishimweyvan90@gmail.com>
2023-06-03 20:03:37 +0200

    add feature

--- resources/views/components/homecss.blade.php
+++ resources/views/components/homecss.blade.php
@@ -1,39 +1,21 @@
+
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <!--====== Required meta tags ======-->
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!--====== Title ======-->
-    <title>{{ env('APP_NAME') }}</title>
-    <!--====== Favicon Icon ======-->
-    <link rel="shortcut icon" href="{{ asset('homepage/images/favicon.ico') }}" type="image/png">
-    <!--====== Google Fonts ======-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
-    <!--====== Flaticon css ======-->
-    <link rel="stylesheet" href="{{ asset('homepage/fonts/flaticon/flaticon_gadden.css') }}">
-    <!--====== FontAwesome css ======-->
-    <link rel="stylesheet" href="{{ asset('homepage/fonts/fontawesome/css/all.min.css') }}">
-    <!--====== Bootstrap css ======-->
-    <link rel="stylesheet" href="{{ asset('homepage/vendor/bootstrap/css/bootstrap.min.css') }}">
-    <!--====== magnific-popup css ======-->
-    <link rel="stylesheet" href="{{ asset('homepage/vendor/magnific-popup/dist/magnific-popup.css') }}">
-    <!--====== Slick-popup css ======-->
-    <link rel="stylesheet" href="{{ asset('homepage/vendor/slick/slick.css') }}">
-    <!--====== Jquery UI css ======-->
-    <link rel="stylesheet" href="{{ asset('homepage/vendor/jquery-ui/jquery-ui.min.css') }}">
-    <!--====== Nice Select css ======-->
-    <link rel="stylesheet" href="{{ asset('homepage/vendor/nice-select/css/nice-select.css') }}">
-    <!--====== Animate css ======-->
-    <link rel="stylesheet" href="{{ asset('homepage/vendor/animate.css') }}">
-    <!--====== Default css ======-->
-    <link rel="stylesheet" href="{{ asset('homepage/css/default.css') }}">
-    <!--====== Style css ======-->
-    <link rel="stylesheet" href="{{ asset('homepage/css/style.css') }}">
-</head>
-
-<body>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>{{env("APP_NAME")}}</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="homepage/css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
