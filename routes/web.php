5b07b349ff7f3b3802c3ec4a83fb1c63e8c838eb
yvan99 <ishimweyvan90@gmail.com>
2023-06-03 18:42:58 +0200

    add feature

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Receptionist login routes
+Route::prefix('receptionist')->group(function () {
+    Route::get('/login', [ReceptionistLoginController::class, 'showLoginForm'])->name('receptionist.login');
+    Route::post('/login', [ReceptionistLoginController::class, 'login'])->name('receptionist.login.submit');
+    // Add other routes specific to the receptionist
+});
+
+// Doctor login routes
+Route::prefix('doctor')->group(function () {
+    Route::get('/login', [DoctorLoginController::class, 'showLoginForm'])->name('doctor.login');
+    Route::post('/login', [DoctorLoginController::class, 'login'])->name('doctor.login.submit');
+    // Add other routes specific to the doctor
+});
+
+// Nurse login routes
+Route::prefix('nurse')->group(function () {
+    Route::get('/login', [NurseLoginController::class, 'showLoginForm'])->name('nurse.login');
+    Route::post('/login', [NurseLoginController::class, 'login'])->name('nurse.login.submit');
+    // Add other routes specific to the nurse
+});
