4cc625f98a2779d434f7c412ed6736cc9a03476a
yvan99 <ishimweyvan90@gmail.com>
2023-06-03 13:51:50 +0200

    feat: create Department.php

---
+++ app/Models/Department.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    use HasFactory;
+
+    public function doctors()
+    {
+        return $this->hasMany(Doctor::class);
+    }
+
+    public function nurses()
+    {
+        return $this->hasMany(Nurse::class);
+    }
+}
