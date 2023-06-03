00f9ff30dfae9d0452a0a425e0c1e3af37f6d399
yvan99 <ishimweyvan90@gmail.com>
2023-06-03 14:01:41 +0200

    consultation migration

---
+++ database/migrations/2023_06_03_120111_create_consultations_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConsultationsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('consultations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_order_id')->constrained('patient_orders');
+            $table->string('code');
+            $table->text('description');
+            $table->foreignId('doctor_id')->constrained('doctors');
+            $table->string('status');
+            $table->foreignId('nurse_id')->constrained('nurses');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('consultations');
+    }
+}
