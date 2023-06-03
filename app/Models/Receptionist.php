23648ee9b26dddf1893308bfb37de27c329db21d
yvan99 <ishimweyvan90@gmail.com>
2023-06-03 13:41:07 +0200

    update Receptionist.php

--- app/Models/Receptionist.php
+++ app/Models/Receptionist.php
@@ -3,9 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Receptionist extends Model
+class Receptionist extends Authenticatable
 {
     use HasFactory;
+
+    protected $fillable = [
+        'names',
+        'email',
+        'phone',
+        'password',
+    ];
 }
