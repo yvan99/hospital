9d7a2dd5f341933590b7eda9c8d40af241a09bb5
yvan99 <ishimweyvan90@gmail.com>
2023-06-04 18:01:52 +0200

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,9 @@ Route::middleware('auth:receptionist')->group(function () {
         Route::get('/departments', [DepartmentController::class, 'index'])->name('departments.index');
         Route::get('/departments/create', [DepartmentController::class, 'create'])->name('departments.create');
         Route::post('/departments', [DepartmentController::class, 'store'])->name('departments.store');
+        Route::get('/doctors/create', [DoctorController::class, 'create'])->name('doctors.create');
+        Route::post('/doctors', [DoctorController::class, 'store'])->name('doctors.store');
+        Route::get('/doctors', [DoctorController::class, 'index'])->name('doctors.index');
     });
 });
 
