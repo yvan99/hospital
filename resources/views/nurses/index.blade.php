f275b11acc640a46e645594768c3bacd1b4b0274
yvan99 <ishimweyvan90@gmail.com>
2023-06-04 18:54:28 +0200

    create 1 file and update 1 file

---
+++ resources/views/nurses/index.blade.php
@@ -0,0 +1,136 @@
+@include('components.dashcss')
+@include('receptionist.components.aside')
+<main class="main-content">
+    <div class="position-relative ">
+        <!--Nav Start-->
+        @include('components.dasheader')
+        <!--Nav End-->
+    </div>
+    <div class="content-inner container-fluid pb-0" id="page_layout">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <div class="header-title">
+                            <h4 class="card-title">Doctors List</h4>
+
+                        </div>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#createDoctorModal">
+                            Register Doctor
+                        </button>
+
+                    </div>
+                    <div class="card-body">
+
+                        <!-- Alert div for displaying messages -->
+                        @if (session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <div class="table-responsive border rounded">
+                            <table id="datatable" class="table " data-toggle="data-table">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
+                                        <th>Department</th>
+                                        <th>Head of Department</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($doctors as $doctor)
+                                        <tr>
+                                            <td>{{ $doctor->id }}</td>
+                                            <td>{{ $doctor->names }}</td>
+                                            <td>{{ $doctor->email }}</td>
+                                            <td>{{ $doctor->phone }}</td>
+                                            <td>{{ $doctor->department->name }}</td>
+                                            <td>{{ $doctor->is_hod ? 'Yes' : 'No' }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+
+                            <!-- Modal for adding doctor -->
+                            <div class="modal fade" id="createDoctorModal" tabindex="-1" role="dialog"
+                                aria-labelledby="createDoctorModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="createDoctorModalLabel">Create Doctor</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="{{ route('doctors.store') }}" method="POST">
+                                                @csrf
+                                                <div class="form-group">
+                                                    <label for="name">Name</label>
+                                                    <input type="text" class="form-control" id="names"
+                                                        name="names" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="email">Email</label>
+                                                    <input type="email" class="form-control" id="email"
+                                                        name="email" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="phone">Phone</label>
+                                                    <input type="text" class="form-control" id="phone"
+                                                        name="phone" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="password">Password</label>
+                                                    <input type="text" class="form-control" id="password"
+                                                        name="password" value="{{ Str::random(12) }}" required>
+                                                    <button type="button" class="btn btn-secondary mt-2"
+                                                        onclick="copyPassword()">Copy</button>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="department_id">Department</label>
+                                                    <select class="form-control" id="department_id" name="department_id"
+                                                        required>
+                                                        <option value="">Select Department</option>
+                                                        @foreach ($departments as $department)
+                                                            <option value="{{ $department->id }}">
+                                                                {{ $department->name }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input" id="is_hod"
+                                                        name="is_hod" value="1">
+                                                    <label class="form-check-label" for="is_hod">Head of
+                                                        Department</label>
+                                                </div>
+                                                <button type="submit" class="btn btn-primary mt-3">Create</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @include('components.dashfooter')
+</main>
+@include('components.dashjs')
--- resources/views/receptionist/components/aside.blade.php
+++ resources/views/receptionist/components/aside.blade.php
@@ -121,8 +121,8 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ Request::is('receptionist/water') ? 'active' : '' }}" aria-current="page"
-                        href="/receptionist/water">
+                    <a class="nav-link {{ Request::is('receptionist/nurses') ? 'active' : '' }}" aria-current="page"
+                        href="/receptionist/nurses">
                         <i class="icon" data-bs-toggle="tooltip" title="UI Elements" data-bs-placement="right">
                             <svg width="20" class="icon-20" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
