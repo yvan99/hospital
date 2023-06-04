22a183d730acc8370543647ffaa0b8d2a39c26ac
yvan99 <ishimweyvan90@gmail.com>
2023-06-04 19:15:07 +0200

    update PatientController.php

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patient;
 use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-    //
+    public function index()
+    {
+        $patients = Patient::all();
+        return view('patients.index', compact('patients'));
+    }
+
+    public function create()
+    {
+        return view('patients.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'code' => 'required|string|unique:patients',
+            'name' => 'required|string|max:255',
+            'gender' => 'required|string',
+            'age' => 'required|integer',
+            'blood_group' => 'required|string',
+            'insurance' => 'nullable|string',
+        ]);
+
+        Patient::create($validatedData);
+
+        return redirect()->route('patients.index')->with('success', 'Patient created successfully.');
+    }
+
+    // Implement edit, update, and delete methods as per your requirements
 }
