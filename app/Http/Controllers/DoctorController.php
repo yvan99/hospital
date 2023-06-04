b7ba9df2d469a3c95d94eba6121954775bfe4ba7
yvan99 <ishimweyvan90@gmail.com>
2023-06-04 18:03:07 +0200

    update DoctorController.php

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('doctors.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validate the request data
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:doctors',
+            'phone' => 'required|string',
+            'password' => 'required|string|min:6',
+            'department_id' => 'required|exists:departments,id',
+            'is_hod' => 'boolean',
+        ]);
+
+        // Create the doctor
+        Doctor::create($validatedData);
+
+        // Redirect to the doctor listing page
+        return redirect()->route('doctors.index')->with('success', 'Doctor created successfully.');
+    }
+
+    public function index()
+    {
+        // Retrieve all doctors
+        $doctors = Doctor::all();
+
+        // Pass the doctors to the view
+        return view('doctors.index', compact('doctors'));
+    }
 }
