e95615d5dfad2a8c6d7b76c5e779fb1e5ebfdb52
yvan99 <ishimweyvan90@gmail.com>
2023-06-04 17:46:26 +0200

    update DepartmentController.php

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class DepartmentController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('departments.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+        ]);
+
+        Department::create($validatedData);
+
+        return redirect()->route('departments.index')->with('success', 'Department created successfully.');
+    }
+
+    public function index()
+    {
+        $departments = Department::all();
+
+        return view('departments.index', compact('departments'));
+    }
 }
