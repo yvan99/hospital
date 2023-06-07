bb5784ef9861d94838f3858688bdabc439264e16
yvan99 <ishimweyvan90@gmail.com>
2023-06-07 23:27:35 +0200

    update aside.blade.php and web.php

--- routes/web.php
+++ routes/web.php
@@ -53,8 +53,8 @@ Route::middleware('auth:receptionist')->group(function () {
 Route::middleware('auth:doctor')->group(function () {
     Route::prefix('doctor')->group(function () {
         Route::view('/dashboard', 'doctor.dashboard');
-        Route::get('/doctor/patient-orders', [DoctorController::class, 'patientOrders'])->name('doctors.patientOrders');
-        Route::post('/doctor/patient-orders/{orderId}/assign', [DoctorController::class, 'assignPatientOrder'])->name('doctors.assignPatientOrder');
+        Route::get('/patient-orders', [DoctorController::class, 'patientOrders'])->name('doctors.patientOrders');
+        Route::post('/patient-orders/{orderId}/assign', [DoctorController::class, 'assignPatientOrder'])->name('doctors.assignPatientOrder');
     });
 });
 
