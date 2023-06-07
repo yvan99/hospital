c3649c8367eeb590cdc5f3ab36d9d482e2950f9c
yvan99 <ishimweyvan90@gmail.com>
2023-06-07 23:25:58 +0200

    update patientOrders.blade.php

--- resources/views/doctor/patientOrders.blade.php
+++ resources/views/doctor/patientOrders.blade.php
@@ -1,5 +1,5 @@
 @include('components.dashcss')
-@include('receptionist.components.aside')
+@include('doctor.components.aside')
 <main class="main-content">
     <div class="position-relative ">
         <!--Nav Start-->
