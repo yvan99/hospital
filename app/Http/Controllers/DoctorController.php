2c5b4a5c0282a474f848acc2d96f17a8d4381f06
yvan99 <ishimweyvan90@gmail.com>
2023-06-07 23:23:42 +0200

    update DoctorController.php

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Consultation;
 use App\Models\Department;
 use App\Models\Doctor;
+use App\Models\PatientOrder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -25,7 +27,7 @@ class DoctorController extends Controller
             'department_id' => 'required|exists:departments,id',
             'is_hod' => 'required|boolean',
         ]);
-    
+
         $validatedData['password'] = Hash::make($validatedData['password']);
         Doctor::create($validatedData);
         return redirect()->route('doctors.index')->with('success', 'Doctor created successfully.');
@@ -37,5 +39,42 @@ class DoctorController extends Controller
         $departments = Department::all();
 
         return view('doctors.index', compact('doctors', 'departments'));
+    }
+
+    public function patientOrders()
+    {
+        // Get the patient orders assigned to the logged-in doctor
+        $doctorId = auth()->user()->id;
+        $patientOrders = PatientOrder::whereHas('consultation', function ($query) use ($doctorId) {
+            $query->where('doctor_id', $doctorId);
+        })->get();
+
+        return view('doctors.patient_orders', compact('patientOrders'));
+    }
+
+    public function assignPatientOrder(Request $request, $orderId)
+    {
+        // Validate the request data
+        $validatedData = $request->validate([
+            'nurse_id' => 'required|exists:nurses,id',
+            'description' => 'required|string',
+        ]);
+
+        // Find the patient order
+        $patientOrder = PatientOrder::findOrFail($orderId);
+
+        // Create a new consultation record
+        $consultation = new Consultation([
+            'patient_order_id' => $patientOrder->id,
+            'description' => $validatedData['description'],
+            'doctor_id' => auth()->user()->id,
+            'nurse_id' => $validatedData['nurse_id'],
+            'status' => 'assigned',
+        ]);
+
+        // Save the consultation
+        $consultation->save();
+
+        return redirect()->route('doctors.patientOrders')->with('success', 'Patient order assigned successfully.');
     }
 }
