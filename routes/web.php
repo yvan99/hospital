5d8e8d45322a6aa1fee7520a1c9bf380dfcf735b
yvan99 <ishimweyvan90@gmail.com>
2023-06-10 14:17:09 +0200

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+}); 
 
 // Receptionist Auth
 Route::prefix('receptionist')->group(function () {
