c34837dcdcbcb4e206a73c3fcddc7e860477974b
yvan99 <ishimweyvan90@gmail.com>
2023-06-10 14:51:30 +0200

    update resources/views/scheduling/index.blade.php

--- resources/views/scheduling/index.blade.php
+++ resources/views/scheduling/index.blade.php
@@ -19,71 +19,8 @@
                     </div>
                     <div class="card-body">
 
-                        <!-- Alert div for displaying messages -->
-                        @if (session('success'))
-                            <div class="alert alert-success">
-                                {{ session('success') }}
-                            </div>
-                        @endif
-
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-                        <div class="table-responsive border rounded">
-                            <table class="table" id="datatable" data-toggle="data-table">
-                                <thead>
-                                    <tr>
-                                        <th>Batch Code</th>
-                                        <th>Consultation Code</th>
-                                        <th>Assigned Doctor</th>
-                                        <th>Patient Info</th>
-                                        <th>Assigned Nurses</th>
-                                        <th>Batch Status</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($patientBatches as $batch)
-                                        <tr>
-                                            <td>{{ $batch->code }}</td>
-                                            <td>{{ $batch->consultation->code }}</td>
-                                            <td>{{ $batch->consultation->doctor->names }}</td>
-                                            <td>
-
-                                                <ul>
-                                                    <li>
-                                                        {{ $batch->consultation->patientOrder->patient->names }}</li>
-                                                    <li>
-                                                        {{ $batch->consultation->patientOrder->patient->code }}</li>
-                                                    <li>
-                                                        {{ $batch->consultation->patientOrder->patient->gender }}</li>
-                                                        <li>
-                                                            {{ $batch->consultation->patientOrder->patient->age }} years</li>
-                                                </ul>
-
-                                            </td>
-                                            <td>
-                                                <ul>
-                                                    @foreach ($batch->nurses as $nurse)
-                                                        <li>{{ $nurse->names }}</li>
-                                                    @endforeach
-                                                </ul>
-                                            </td>
-                                            <td>
-                                                <button class="btn btn-warning btn-sm">
-                                                    {{$batch->status}}
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                       
+                 
                     </div>
                 </div>
             </div>
