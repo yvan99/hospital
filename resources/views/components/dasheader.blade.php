826046f7b2123168ae33be3911d0cd720a042116
yvan99 <ishimweyvan90@gmail.com>
2023-06-11 15:58:55 +0200

    update dasheader.blade.php

--- resources/views/components/dasheader.blade.php
+++ resources/views/components/dasheader.blade.php
@@ -36,59 +36,11 @@
         </div>
         <div class="d-flex align-items-center justify-content-between product-offcanvas">
             <div class="breadcrumb-title border-end me-3 pe-3 d-none d-xl-block">
-                <small class="mb-0 text-capitalize">Home</small>
+                <small class="mb-0 text-capitalize">Dashboard</small>
             </div>
             <div class="offcanvas offcanvas-end shadow-none iq-product-menu-responsive" tabindex="-1"
                 id="offcanvasBottom">
-                <div class="offcanvas-body">
-                    <ul class="iq-nav-menu list-unstyled">
-                        <li class="nav-item ">
-                            <a class="nav-link menu-arrow justify-content-start active"
-                                data-bs-toggle="collapse" href="#homeData" role="button" aria-expanded="false"
-                                aria-controls="homeData">
-                                <svg class="icon-20" width="20" viewBox="0 0 24 24" fill="currentColor"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M9.14373 20.7821V17.7152C9.14372 16.9381 9.77567 16.3067 10.5584 16.3018H13.4326C14.2189 16.3018 14.8563 16.9346 14.8563 17.7152V20.7732C14.8562 21.4473 15.404 21.9951 16.0829 22H18.0438C18.9596 22.0023 19.8388 21.6428 20.4872 21.0007C21.1356 20.3586 21.5 19.4868 21.5 18.5775V9.86585C21.5 9.13139 21.1721 8.43471 20.6046 7.9635L13.943 2.67427C12.7785 1.74912 11.1154 1.77901 9.98539 2.74538L3.46701 7.9635C2.87274 8.42082 2.51755 9.11956 2.5 9.86585V18.5686C2.5 20.4637 4.04738 22 5.95617 22H7.87229C8.19917 22.0023 8.51349 21.8751 8.74547 21.6464C8.97746 21.4178 9.10793 21.1067 9.10792 20.7821H9.14373Z"
-                                        fill="currentColor" />
-                                </svg>
-                                <span class="nav-text ms-2">{{env("APP_NAME")}}</span>
-                            </a>
-                            {{-- <ul class="iq-header-sub-menu list-unstyled collapse" id="homeData">
-                                <li class="nav-item"><a class="nav-link active" href="index.html">Dashboard</a>
-                                </li>
-                                <li class="nav-item"><a class="nav-link "
-                                        href="analytics-dashboard.html">Analytics</a></li>
-                                <li class="nav-item"><a class="nav-link "
-                                        href="crypto-dashboard.html">Crypto</a></li>
-                                <li class="nav-item">
-                                    <a class="nav-link menu-arrow" data-bs-toggle="collapse" href="#menuStyles"
-                                        role="button" aria-expanded="false" aria-controls="menuStyles">
-                                        Menu Style
-                                        <i class="right-icon">
-                                            <svg class="icon-20" width="20" viewBox="0 0 24 24"
-                                                fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M8.5 5L15.5 12L8.5 19" stroke="currentColor"
-                                                    stroke-width="1.5" stroke-linecap="round"
-                                                    stroke-linejoin="round"></path>
-                                            </svg>
-                                        </i>
-                                    </a>
-                                    <ul aria-expanded="false"
-                                        class="iq-header-sub-menu left list-unstyled collapse" id="menuStyles">
-                                        <li class="nav-item"><a class="nav-link "
-                                                href="index-horizontal.html">Horizontal Dashboard</a></li>
-                                        <li class="nav-item"><a class="nav-link "
-                                                href="index-dual-compact.html">Dual Compact</a></li>
-                                        <li class="nav-item"><a class="nav-link "
-                                                href="index-boxed.html">Boxed Horizontal</a></li>
-                                    </ul>
-                                </li>
-                            </ul> --}}
-                        </li>
-                    
-                    </ul>
-                </div>
+        
             </div>
         </div>
         <div class="d-flex align-items-center">
