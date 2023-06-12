543d80466abdaba9b028dfe7580ac823444f9ec9
yvan99 <ishimweyvan90@gmail.com>
2023-06-12 23:55:24 +0200

    update Note.php

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['patient_batch_id', 'nurse_id', 'doctor_id', 'message'];
+
+    public function patientBatch()
+    {
+        return $this->belongsTo(PatientBatch::class);
+    }
+
+    public function nurse()
+    {
+        return $this->belongsTo(Nurse::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 }
