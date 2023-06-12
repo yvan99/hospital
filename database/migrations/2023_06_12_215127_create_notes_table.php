78f3db0326799a1e093886dc1f380ca6ec25a2c8
yvan99 <ishimweyvan90@gmail.com>
2023-06-12 23:52:05 +0200

    update 2023_06_12_215127_create_notes_table.php

--- database/migrations/2023_06_12_215127_create_notes_table.php
+++ database/migrations/2023_06_12_215127_create_notes_table.php
@@ -1,29 +1,22 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateNotesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('patient_batch_id')->constrained()->onDelete('cascade');
+            $table->foreignId('nurse_id')->constrained()->onDelete('cascade');
+            $table->foreignId('doctor_id')->constrained()->onDelete('cascade');
+            $table->text('message');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('notes');
