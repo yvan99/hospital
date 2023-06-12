dbebf8d6d912c0b781b0885cadba4d0c25df2a43
yvan99 <ishimweyvan90@gmail.com>
2023-06-13 00:23:12 +0200

    update NoteController.php

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
-use App\Models\PatientBatch;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
@@ -17,8 +16,8 @@ class NoteController extends Controller
 
         Note::create([
             'patient_batch_id' => $request->input('patient_batch_id'),
-            'nurse_id' => auth()->user()->id,
-            'doctor_id' => PatientBatch::find($request->input('patient_batch_id'))->doctor_id,
+            'user_name' => auth()->user()->name,
+            'user_type' => auth()->getDefaultDriver(),
             'message' => $request->input('message'),
         ]);
 
