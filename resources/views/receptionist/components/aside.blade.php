cf5f08a786b4d2f0f07d5d82fe44e431207e0916
yvan99 <ishimweyvan90@gmail.com>
2023-06-13 02:03:05 +0200

    update aside.blade.php

--- resources/views/receptionist/components/aside.blade.php
+++ resources/views/receptionist/components/aside.blade.php
@@ -51,7 +51,7 @@
                             data-bs-placement="right">-</span>
                     </a>
                 </li>
-                <li class="nav-item">
+                {{-- <li class="nav-item">
                     <a class="nav-link {{ Request::is('receptionist/dashboard') ? 'active' : '' }}" aria-current="page"
                         href="/receptionist/dashboard">
                         <i class="icon" data-bs-toggle="tooltip" title="Dashboard" data-bs-placement="right">
@@ -67,11 +67,11 @@
                         </i>
                         <span class="item-name">Dashboard</span>
                     </a>
-                </li>
+                </li> --}}
 
                 <li class="nav-item">
-                    <a class="nav-link {{ Request::is('receptionist/departments') ? 'active' : '' }}" aria-current="page"
-                        href="/receptionist/departments">
+                    <a class="nav-link {{ Request::is('receptionist/dashboard') ? 'active' : '' }}" aria-current="page"
+                        href="/receptionist/dashboard">
                         <i class="icon" data-bs-toggle="tooltip" title="Utilities" data-bs-placement="right">
                             <svg width="20" class="icon-20" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
