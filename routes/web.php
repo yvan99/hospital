8263b8848679f898dd3aba86017546c9a8d35e33
patience-man <hseal419@gmail.com>
2023-09-12 07:07:38 +0200

    chore: change the redirect route for unauthorized case

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('homepage');
 
 // Receptionist Auth
 Route::prefix('receptionist')->group(function () {
